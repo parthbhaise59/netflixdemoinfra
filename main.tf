f79915e792c52618a2a0755c3806e1e8f339d60b
parthbhaise59 <109677620+parthbhaise59@users.noreply.github.com>
2025-03-26 13:01:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ count = 4
 ami = "ami-0ddc798b3f1a5117e"
 instance_type = "t2.medium"
 key_name = "server2"
-vpc_security_group_ids = ["sg-05f044979e305302e"]
+vpc_security_group_ids = ["sg-0868944b0dc51febb"]
 tags = {
 Name = var.instance_names[count.index]
 }
